d51e091566dd0ef2bf2c53178132c0fd96201b06
estradahamma22 <56485389+estradahamma22@users.noreply.github.com>
2022-03-28 18:42:28 -0400

    made change to make work

--- src/main/java/org/piki/App.java
+++ src/main/java/org/piki/App.java
@@ -13,7 +13,7 @@ public class App {
 
         long a = 1, b = 1;
         for (int i=2; i<n; i++) {
-            long t = a;
+            int t = a;
             a = b;
             b = b + t;
         }
